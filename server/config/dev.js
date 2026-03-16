939f5ee2a558bcc9f85b96664012e88111a5b9a2
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-03-16 19:24:48 -0300

    *Started backend enhancements by migrating to TS, setting tsconfig.json and installing necessary dependencies.     * Deleted old JS files and updated .gitignore to exclude private key files (dev.js, prod.js, keys.js).     * package.json:         - Updated scripts to run src/index.ts (not in this commit) instead of index.js         - Updated scripts routes to match new project structure (client and server folders)         - Dependencies added:             - morgan for logging HTTP requests             - types for cookie-session, morgan, node, passport, passport-google-oauth20.             - redis for caching         - Updated packages:             - mongoose updated to latest version             - cookie-session updated to latest version

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	googleClientID:
-		"70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-	googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-	mongoURI:
-		"mongodb+srv://tomascomputerguy:8v6plvbAGtNGo5SW@cluster0.cnodi5i.mongodb.net/advanced_node?retryWrites=true&w=majority&appName=advanced-nodejs",
-	cookieKey: "123123123",
-};
