633b61b699fd36333665ccf87bccb8d31b15a17c
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-04-12 16:14:44 -0400

    Fix CI: update actions to v4, add moduleFileExtensions to Jest, add server startup validation

--- server/jest.config.js
+++ server/jest.config.js
@@ -10,6 +10,7 @@ export default {
     moduleNameMapper: {
         "^(\\.{1,2}/.*)\\.js$": "$1",
     },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     transform: {
         "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
     },
